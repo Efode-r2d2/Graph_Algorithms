b743b2efed89c691f7d6bd15d83ff19fac0f3da8
EFRIEM-DESALEW <efidesalew@gmail.com>
2018-08-02 16:23:41 +0300

    Breadth First Search

--- BFS.cpp
+++ BFS.cpp
@@ -9,6 +9,7 @@
 #include<queue>
 
 using namespace std;
+
 /* used to store the graph */
 vector<int> v[10];
 /* required to store the level of the nodes*/
@@ -17,7 +18,10 @@ int level[10];
 bool vis[10];
 /* breadth first search algorithm */
 void bfs(int source);
+/* display the grpah */
 void display(int nodes);
+/* print level*/
+void print_level();
 int main(){
 	int x,y,nodes,edges;
 	/* accepting the number of nodes from the user */
@@ -28,18 +32,23 @@ int main(){
 	cin>>edges;
 	/* accepting the graph from the user */
 	for(int i=0;i<nodes;i++){
-		int x=0;
+		int y=0;
 		do{
 			cout<<"Enter nodes connected to node"<<i+1<<endl;
 			cin>>x;
-			v[i].push_back(x-1);
+			if(x>0)
+				v[i].push_back(x-1);
 			cout<<"Enter 0 to move to another node."<<endl;
-			cin>>x;
-		}while(x!=0);
+			cin>>y;
+		}while(y!=0);
 	}
 	/* displaying the graph accepted from the user */
 	display(nodes);
+	/* calling breadth first search algorithm */
 	bfs(0);
+	/* print level after bfs*/
+	cout<<"The Level of the Nodes \n";
+	print_level();
 	return 0;
 }
 /* breadth first search algorithm */
@@ -63,6 +72,7 @@ void bfs(int source){
 		}
 	}
 }
+/* display the graph */
 void display(int nodes){
 	for(int i=0;i<nodes;i++){
 		cout<<"Node "<<i+1<<":->";
@@ -72,4 +82,8 @@ void display(int nodes){
 		}	
 		cout<<endl;
 	}
+}
+void print_level(){
+	for(int i=0;i<10;i++)
+		cout<<level[i]<<endl;
 }
